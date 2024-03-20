c5d536303220e6dac8840cceef141c2ede841465
Boodenator <boode1000@gmail.com>
2024-03-20 07:00:49 -0700

    task2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    sum = 0
+    for digit in my_list:
+        if digit not in new_list:
+            sum += digit
+            new_list.append(digit)
+
+    return sum
+
+            
+
