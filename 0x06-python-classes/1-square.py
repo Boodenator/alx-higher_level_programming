af9636e4026ffd5668d91548f8b131d06f485244
Boodenator <boode1000@gmail.com>
2024-04-02 16:07:03 -0700

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define the class Square."""
+
+
+class Square:
+    """Represent the square."""
+
+    def __init__(self, size):
+        """New Square initialization.
+        Args:
+            size (int): The new square size.
+        """
+        self.__size = size
