3de9fe8e131f90b7841050a7f8d1bd782aeb48de
Boodenator <boode1000@gmail.com>
2024-04-15 14:55:51 -0700

    4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-''' module: 4-inherits_from '''
+''' module: 4-inherits-from '''
 
 
 def inherits_from(obj, a_class):
-    '''the object is an instance of a class inherited (directly or indirectly)
-    obj: an object
-    a_class: a class
+    '''the object is an instance of a class inherited (indirectly or directly)
+    obj: is an object
+    a_class: is a class
     returns None
     '''
     return type(obj) != a_class and isinstance(obj, a_class)
