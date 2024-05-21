2005d56a7a2503ee45948c837c2ea74c9f776f4f
Boodenator <boode1000@gmail.com>
2024-05-21 14:48:52 -0700

    task15

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- script to list the records numbers with the same score in the table second_table of the database hbtn_0c_0
+SELECT score,
+	COUNT(score) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
