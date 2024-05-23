bfe283bdca16d7582187d72d31e0d7ad22221f8c
Boodenator <boode1000@gmail.com>
2024-05-23 14:58:20 -0700

    task20

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,9 @@
+-- Script to view genres among the database hbtn_0d_tvshows_rate by their rating.
+
+SELECT tv_genres.name,
+	SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+	JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+	JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
+ORDER BY rating DESC
+GROUP BY tv_genres.name;
