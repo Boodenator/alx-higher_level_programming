03d409a3b18d7a6fff77cb5f9e8acecc79911606
Boodenator <boode1000@gmail.com>
2024-05-23 14:30:49 -0700

    task9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- script for displaying all cities included in the database hbtn_0d_usa
+
+SELECT cities.id,
+	cities.name,
+	states.name
+FROM cities
+	JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
