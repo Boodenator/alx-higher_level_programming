175643ad30cecef8ce14765a3c6b4d016eeb171a
Boodenator <boode1000@gmail.com>
2024-06-23 05:25:41 -0700

    task6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a class of a State with an instance
+Base = declarative_base() """
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Inheritition of state class from Base declarative_base()
+    linking the MySQL table states
+    Attr:
+        id, name
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
