d3834f0b64c1354128eb5a6bae33f7b5270ea03f
Boodenator <boode1000@gmail.com>
2024-07-05 15:48:43 -0700

    task10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script which
+1-input user's GitHub credentials
+2-uses the GitHub API to view your id
+"""
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
