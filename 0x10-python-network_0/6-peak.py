8beaf89b518ff0f614f18fffef6d3d7bca4a8ab1
Boodenator <boode1000@gmail.com>
2024-07-09 05:16:42 -0700

    fx2 task6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """Finding a peak"""
 
-def find_peak(list_of_integers):
 
-    loi = list_of_integers
-    if loi == []:
+def find_peak(list_of_integers):
+    """Finding a peak for list_of_integers"""
+
+    if list_of_integers is None or list_of_integers == []:
         return None
-    total = len(loi)
-    if total == 1:
-        return loi[0]
-    elif total == 2:
-        return max(loi)
-    middle = int(total / 2)
-    peak = loi[middle]
-    if peak > loi[middle - 1] and peak > loi[middle + 1]:
-        return peak
-    elif peak < loi[middle - 1]:
-        return find_peak(loi[:middle])
-    else:
-        return find_peak(loi[middle + 1:])
+    lo = 0
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
+    mid = int(mid)
+    if hi == 1:
+        return list_of_integers[0]
+    if hi == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
