01268b72906c20e073a91792b6a2394895940ccf
Boodenator <boode1000@gmail.com>
2024-07-09 05:28:54 -0700

    task9 fx

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Forming request to 0.0.0.0:5000/catch_me which returns message "You got me!".
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: ALXSWE" -d "user_id=98" 0.0.0.0:5000/catch_me
